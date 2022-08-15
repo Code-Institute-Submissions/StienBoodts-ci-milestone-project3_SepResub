1bcb5ac9b4632bcb64744ae8ff01978e086144d7
StienBoodts <stien.boodts@gmail.com>
2022-08-15 19:27:48 +0000

    Create database with psql and migrate with Python 3

--- surfproject/models.py
+++ surfproject/models.py
@@ -6,24 +6,24 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(25), unique=True, nullable=False)
     user_email = db.Column(db.String(100), unique=True, nullable=False)
-    user_password = db.Column(db.String(50), unique=True, nullable=False)
+    user_password = db.Column(db.String(50), nullable=False)
+    reviews = db.relationship("Review", backref="user", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
         return self.user_name
 
 
-    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
-
-
 class Review(db.Model):
     #schema for the reviews
     id = db.Column(db.Integer, primary_key=True)
-    camp_name = db.Column(db.String(100), unique=True, nullable=False)
-    camp_id = db.Column(db.Integer, db.ForeignKey("camp.id", ondelete="CASCADE"), nullable=False)
+    camp_name = db.Column(db.String(100), nullable=False)
+    camp_id = db.Column(db.Integer, db.ForeignKey("camps.id", ondelete="CASCADE"), nullable=False)
     camp_rating = db.Column(db.Integer, nullable=False)
-    user_name = db.Column(db.Integer, db.ForeignKey("user.user_name", ondelete="CASCADE"), nullable=False)
+    user_name = db.Column(db.String(25), db.ForeignKey("user.user_name", ondelete="CASCADE"), nullable=False)
     review_text = db.Column(db.Text, nullable=False)
+    camps = db.relationship("Camps", backref="review", cascade="all, delete", lazy=True)
+
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
@@ -36,11 +36,21 @@ class Countries(db.Model):
     #schema for the countries used in overview per country
     id = db.Column(db.Integer, primary_key=True)
     country_name = db.Column(db.String(30), nullable=False)
+    campcountries = db.relationship("Camps", backref="countries", cascade="all, delete", lazy=True)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.country_name
 
 
 class Camps(db.Model):
     #schema for the camps overview
     id = db.Column(db.Integer, primary_key=True)
-    average_rating = (db.Integer, nullable=False)
-    camp_name = (db.String, db.ForeignKey("review.camp_name", ondelete="CASCADE"), nullable=False)
-    country_name = (db.String, db.ForeignKey("countries.country_name", ondelete="CASCADE"), nullable=False)
+    average_rating = (db.Integer)
+    camp_name = (db.String, db.ForeignKey("review.camp_name", ondelete="CASCADE"))
+    country_name = (db.String, db.ForeignKey("countries.country_name", ondelete="CASCADE"))
+    campreviews = db.relationship("Review", backref="camps", cascade="all, delete", lazy=True)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.camp_name
--- surfproject/routes.py
+++ surfproject/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template
-from surfproject import app,db
-
+from surfproject import app, db
+from surfproject.models import User, Review, Countries, Camps
 
 @app.route("/")
 def home():
