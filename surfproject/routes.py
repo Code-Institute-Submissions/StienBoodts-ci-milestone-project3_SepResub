4cf7ec7ccf2612b9b9e960aee003d7a2c1400205
StienBoodts <stien.boodts@gmail.com>
2022-08-15 22:06:10 +0000

    Add content to home page

--- surfproject/routes.py
+++ surfproject/routes.py
@@ -4,4 +4,16 @@ from surfproject.models import User, Review, Countries, Camps
 
 @app.route("/")
 def home():
+    return render_template("home.html")
+
+@app.route("/camps")
+def camps():
     return render_template("camps.html")
+
+@app.route("/countries")
+def countries():
+    return render_template("countries.html")
+
+@app.route("/new_review")
+def new_review():
+    return render_template("new_review.html")
